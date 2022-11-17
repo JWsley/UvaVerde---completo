6c73dfa35def0f703d5841cb50ee6c294ef2aff0
“Jonathan <jonathanwleycijani@gmail.com>
2022-11-17 16:55:21 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function location(){
+
+    window.open('https://www.google.com.br/maps/place/Uva+Verde+Moda+Infantil/@-22.2300224,-45.9362798,17z/data=!3m1!4b1!4m5!3m4!1s0x94cbc72f7e35ab71:0x5f300283f8c26521!8m2!3d-22.2300475!4d-45.9341168')
+
+}
